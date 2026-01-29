34d3c25ceb1d1ea08c3ed67803e79c47d96ace3f
Alex Olsson <a1exnd3r@node42.dev>
2026-01-29 10:23:29 +0100

    wip: add tests and eslint setup

---
+++ test/utils.test.js
@@ -0,0 +1,27 @@
+const { buildDocLabel } = require("../src/utils");
+const { expect } = require("chai");
+
+describe("buildDocLabel", () => {
+  it("formats wildcard invoice", () => {
+    const doc = {
+        scheme: "peppol-doctype-wildcard",
+        value: "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##*"
+    };
+
+    expect(buildDocLabel(doc)).to.equal("Any Invoice (Wildcard)");
+  });
+
+  it("formats BIS invoice", () => {
+    const doc = {
+      scheme: "busdox-docid-qns",
+      value: "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2::Invoice##urn:cen.eu:en16931:2017"
+    };
+
+    expect(buildDocLabel(doc)).to.equal("Invoice (BIS 3)");
+  });
+
+  it("falls back on unknown value", () => {
+    const doc = { scheme: "busdox-docid-qns", value: "unknown" };
+    expect(buildDocLabel(doc)).to.equal("Document");
+  });
+});
